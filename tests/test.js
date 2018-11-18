a2788d332ad786bc859d0e1f48be8789a9e716f1
bahalool <bahalool@gmail.com>
2018-11-18 14:32:52 +0200

    support storage manager

---
+++ tests/test.js
@@ -0,0 +1,39 @@
+
+const { expect } = require('chai');
+const etcd = require('../index');
+const uuidv4 = require('uuid/v4');
+const moment = require('moment');
+const path = require('path');
+const DateFormat = 'YYYY-MM-DD';
+
+describe('etcd', () => {
+    beforeEach(async () => {
+        const config = {};
+        config.serviceName = 'etcd-adapter';
+
+        config.etcd = {
+            protocol: 'http',
+            host: process.env.ETCD_CLIENT_SERVICE_HOST || 'localhost',
+            port: process.env.ETCD_CLIENT_SERVICE_PORT || 4001
+        };
+
+        await etcd.init(config, null, null, true);
+    });
+    describe('get/set', () => {
+        it('should set a key and then get the same key', async () => {
+            const jobId = uuidv4();
+            const taskId = uuidv4();
+            const data = { data: 'yes' };
+            const res = await etcd.put({ Path: path.join('hkube', moment().format(DateFormat), jobId, taskId), Data: data });
+            const result = await etcd.get(res);
+            expect(result).to.deep.equal(data);
+        });
+        it('should set a key and then get the same key - result', async () => {
+            const jobId = uuidv4();
+            const data = { data: 'yes' };
+            const res = await etcd.put({ Path: path.join('hkube-results', moment().format(DateFormat), jobId), Data: data });
+            const result = await etcd.get(res);
+            expect(result).to.deep.equal(data);
+        });
+    });
+});
